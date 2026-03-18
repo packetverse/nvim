6ca646842c559cff08580a036fd929a051faf236
packetverse <60551416+packetverse@users.noreply.github.com>
2026-03-18 21:41:24 +0100

    style(lazy): apply formatting changes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,11 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+---@diagnostic disable-next-line: undefined-field
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
+      { out, "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -22,7 +24,7 @@ require("lazy").setup({
     enabled = false,
   },
   change_detection = {
-    notify = false
+    notify = false,
   },
   performance = {
     rtp = {
@@ -36,7 +38,7 @@ require("lazy").setup({
         "tutor",
         "spellfile",
         "zipPlugin",
-      }
-    }
-  }
+      },
+    },
+  },
 })
