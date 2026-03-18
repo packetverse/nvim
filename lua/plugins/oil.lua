9dbcc5cc3559796f853c2abca549b00198451330
packetverse <60551416+packetverse@users.noreply.github.com>
2026-03-18 22:31:58 +0100

    feat(plugins/oil): change window layout, add highlight groups, and change keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,16 +2,20 @@ return {
   "stevearc/oil.nvim",
   lazy = false,
   dependencies = {
-    "nvim-mini/mini.icons", opts = {},
+    "nvim-mini/mini.icons",
+    opts = {},
   },
   opts = {
     default_file_explorer = true,
     columns = { "icon" },
     keymaps = {
-      ["<C-h>"] = false,
+      ["<C-h>"] = "actions.toggle_hidden",
+      ["<C-t>"] = "actions.toggle_trash",
       ["<C-c>"] = false,
-      ["<M-h>"] = "actions.select_split",
+      ["<C-k>"] = false,
       ["q"] = "actions.close",
+      ["<C-f>"] = "actions.preview_scroll_down",
+      ["<C-b>"] = "actions.preview_scroll_up",
     },
     delete_to_trash = true,
     view_options = {
@@ -22,10 +26,38 @@ return {
     float = {
       max_width = 0.8,
       max_height = 0.8,
-      border = "single",
-    }
+      border = "rounded",
+      win_options = {
+        winhighlight = "FloatTitle:OilFloatTitle,FloatBorder:OilFloatBorder",
+      },
+      get_win_title = function()
+        return (" %s "):format(vim.fn.fnamemodify(require("oil").get_current_dir(), ":~"))
+      end,
+      override = function(conf)
+        conf.title = (" %s "):format(vim.fn.fnamemodify(require("oil").get_current_dir(), ":~"))
+        conf.title_pos = "center"
+        return conf
+      end,
+    },
+    preview_win = {
+      preview_method = "scratch",
+      win_options = {
+        winhighlight = "FloatTitle:OilPreviewTitle,FloatBorder:OilPreviewBorder",
+      },
+    },
   },
   keys = {
-    { "-", function() require("oil").toggle_float() end, desc = "Open parent directory" }
+    {
+      "-",
+      function()
+        require("oil").toggle_float()
+      end,
+      desc = "Open parent directory (float)",
+    },
+    {
+      "<leader>-",
+      "<CMD>Oil<CR>",
+      desc = "Open parent directory",
+    },
   },
 }
