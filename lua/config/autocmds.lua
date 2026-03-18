bb2d7d3c73c009b3ba68fa4d85d6a2bf96e06f35
packetverse <60551416+packetverse@users.noreply.github.com>
2026-03-18 22:21:57 +0100

    fix(autocmds): remove broken autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     vim.opt_local.formatoptions:remove("r")
     vim.opt_local.formatoptions:remove("o")
-  end
+  end,
 })
 
 -- Highlight yank
@@ -16,19 +16,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     })
   end,
 })
-
--- BUG: Seems to be not working as of now
--- Automatically reload config
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = vim.fn.stdpath("config") .. "/lua/**/*.lua",
-  callback = function()
-    local init_path = vim.fn.stdpath("config") .. "/init.lua"
-    local ok, err = pcall(dofile, init_path)
-    if not ok then
-      print("Error reloading config: " .. err)
-    else
-      print("Config reloaded successfully!")
-    end
-  end
-})
-
